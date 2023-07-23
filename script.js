010b72f01fe77cd267d7b435e45fad1f1b5d7b5d
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-07-23 18:35:31 +0200

    I have added function that gets a number of userinput and create divs that are necessary to make equal col and row grids i.e[16x16] and also css file has been updated to make container display grid

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded',() =>{
- 
+    getGridItems(16)
+    let selectBtn = document.querySelector('.select-btn')
+    selectBtn.addEventListener('click',() => {
+        let choice = getPopup();
+        getGridItems(choice)
+    })
+   
 })
 function getPopup(){
     let userInput = prompt('Choose the size of grids');
@@ -12,7 +18,23 @@ function getPopup(){
         popupMessage.textContent = 'Text are not allowed! use number only.'    
     }else{
         popupMessage.textContent = 'Now you can play'
+        return userInput
     }
-
 }
 
+function getGridItems (size){
+    let gridContainer = document.querySelector('.container');
+    for(i = gridContainer.children.length - 1; i >= 0; i--){
+        let child = gridContainer.children[i]
+        gridContainer.removeChild(child)
+    };
+    let numDiv = size * size;
+
+    for (i= 1; i <= numDiv; i++){
+        let div = document.createElement('div');
+        gridContainer.appendChild(div);
+        div.addEventListener('mouseover',() => div.style.backgroundColor = 'black');       
+    }
+    
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
